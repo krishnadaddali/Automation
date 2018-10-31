51dfc6b01f20f8add2c3e6d644c554be56da9012
krishna <krishna.01718@gmail.com>
2018-10-31 17:54:51 -0400

    SearchPageUpdated

--- src/main/java/com/wfo/card/pojo/SearchPage.java
+++ src/main/java/com/wfo/card/pojo/SearchPage.java
@@ -6,4 +6,41 @@ public class SearchPage {
 	private String name;
 	private String location;
 
+	public SearchPage(int id, String name, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchPage [id=" + id + ", name=" + name + ", location="
+				+ location + "]";
+	}
+
 }
